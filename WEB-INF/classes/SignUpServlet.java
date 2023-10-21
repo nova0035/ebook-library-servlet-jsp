4d42bf11c11e0f90715fe5b38ded39ac64a00a6a
nova0035 <padaliyabhavya834@gmail.com>
2023-10-21 09:20:59 +0530

    SIGN UP SERVLET CODE UPDATED

--- WEB-INF/classes/SignUpServlet.java
+++ WEB-INF/classes/SignUpServlet.java
@@ -1,15 +1,7 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
 
 public class SignUpServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
